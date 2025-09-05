2be2b1920af14f9f4cdb6905be76ed2aaac70b8f
Lam A2 dep trai so 1 TG <135971056+Iz-HaruVN@users.noreply.github.com>
2025-09-05 16:22:57 +0700

    Add files via upload

---
+++ csharp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ScreenMagnifier
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out Point lpPoint);
+        [DllImport("user32.dll")]
+        private static extern IntPtr MonitorFromPoint(Point pt, uint dwFlags);
+        [DllImport("shcore.dll")]
+        private static extern int GetDpiForMonitor(IntPtr hmonitor, int dpiType, out uint dpiX, out uint dpiY);
+
+        private Timer timer;
+        private PictureBox pictureBox;
+        private const int captureSize = 100;
+        private const int zoom = 3;
+
+        public Form1()
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.TopMost = true;
+            this.Width = captureSize * zoom;
+            this.Height = captureSize * zoom;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(10, 10);
+
+            // Tạo PictureBox
+            pictureBox = new PictureBox
+            {
+                Dock = DockStyle.Fill,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            this.Controls.Add(pictureBox);
+
+            // Tạo Timer
+            timer = new Timer { Interval = 20 };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private Point GetPhysicalCursorPos()
+        {
+            // Luôn trả về tọa độ desktop gốc
+            GetCursorPos(out Point pt);
+            return pt;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Point cursor = GetPhysicalCursorPos();
+
+                Rectangle rect = new Rectangle(
+                    cursor.X - captureSize / 2,
+                    cursor.Y - captureSize / 2,
+                    captureSize,
+                    captureSize
+                );
+
+                // Giới hạn trong screen hiện tại
+                Screen scr = Screen.FromPoint(cursor);
+                rect.Intersect(scr.Bounds);
+
+                if (rect.Width <= 0 || rect.Height <= 0) return;
+
+                using (Bitmap bmp = new Bitmap(rect.Width, rect.Height))
+                {
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.CopyFromScreen(rect.Location, Point.Empty, rect.Size);
+                    }
+
+                    Bitmap zoomed = new Bitmap(bmp, new Size(rect.Width * zoom, rect.Height * zoom));
+                    pictureBox.Image?.Dispose();
+                    pictureBox.Image = zoomed;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Text = $"Lỗi: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ csharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("csharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("csharp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4989a666-356b-43c7-a2e5-0cef8e56a8c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
